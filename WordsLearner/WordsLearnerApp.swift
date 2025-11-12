9bb9e0797ea547d931869ebab6324df4e08ee525
Jeffrey <jeffreyzeng@wicresoft.com>
2025-11-12 12:54:42 +0800

    Initial Commit

---
+++ WordsLearner/WordsLearnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsLearnerApp.swift
+//  WordsLearner
+//
+//  Created by Jeffrey on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WordsLearnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
