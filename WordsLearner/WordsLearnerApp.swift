e7021334ef8a9725c6d34919a4cb60ac795df84a
Jeffrey <jjjjjeffreyzeng@gmail.com>
2025-12-09 13:19:45 +0800

    prepare dependencies with test value in debug environment

--- WordsLearner/WordsLearnerApp.swift
+++ WordsLearner/WordsLearnerApp.swift
@@ -7,13 +7,19 @@
 
 import SwiftUI
 import ComposableArchitecture
+import SQLiteData
 
 @main
 struct EnglishWordComparatorApp: App {
     init() {
         // Bootstrap database on app launch
         try! prepareDependencies {
+            #if DEBUG
+            $0.defaultDatabase = .testDatabase
+            $0.comparisonGenerator = .testValue
+            #else
             try $0.bootstrapDatabase()
+            #endif
         }
         
         // Start background task processing
